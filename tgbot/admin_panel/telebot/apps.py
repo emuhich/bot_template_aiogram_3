5c5cad76a93959f5a27a1970e7532f7b56f06097
Egor Mukhametvafin <egor190420000@gmail.com>
2022-10-03 06:58:00 +0300

    Delete tgbot directory

--- tgbot/admin_panel/telebot/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TelebotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_panel.telebot'
-    verbose_name = 'Управление ботом'
--- tgbot/misc/states.py
+++
@@ -1,5 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class Test(StatesGroup):
-    state_1 = State()
